9d51becfef17376c5a6f6607d10e001821157d7a
GaromaMakure <garomamak11@gmail.com>
2024-12-17 11:35:11 -0800

    adding header scroll using toggle method in javascript

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// header scroll
+let header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
 // swiper
 var swiper = new Swiper(".home", {
   spaceBetween: 30,
